51736dd435373b4b5226603cf33d35b5d2d3ff0d
jiaojiaojiaoCpbr <jiaojiaojiao.cpbr@gmail.com>
2020-12-31 19:09:55 +0800

    replace format to usd

--- ch1/statement.js
+++ ch1/statement.js
@@ -35,14 +35,17 @@ function statement(invoice, plays) {
     return result;
   };
 
+  const usd = (aNumber) => {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber);
+  };
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const { format } = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  });
 
   for (let i = 0; i < invoice.performances.length; i += 1) {
     const perf = invoice.performances[i];
@@ -50,12 +53,12 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
