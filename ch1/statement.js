b37613258e8217b463d6cdc62e4b7969f09f2434
jiaojiaojiaoCpbr <jiaojiaojiao.cpbr@gmail.com>
2021-01-04 14:38:13 +0800

    move function: amountFor

--- ch1/statement.js
+++ ch1/statement.js
@@ -78,9 +78,32 @@ const renderPlainText = (data, plays) => {
 const statement = (invoice, plays) => {
   const playFor = (performance) => plays[performance.playID];
 
+  const amountFor = (performance) => {
+    let result = 0;
+    switch (performance.play.type) {
+      case 'tragedy':
+        result = 40000;
+        if (performance.audience > 30) {
+          result += 1000 * (performance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (performance.audience > 20) {
+          result += 10000 + 500 * (performance.audience - 20);
+        }
+        result += 300 * performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${performance.play.type}`);
+    }
+    return result;
+  };
+
   const enrichPerformance = (performance) => {
     const result = { ...performance };
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   };
 
